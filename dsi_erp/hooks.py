a63ecd24d722a122f4c9010c638ed5cc8809e8f1
Ramees-Enfono <ramees@enfono.in>
2025-08-06 23:17:37 +0300

    employee documents,renewable documents notification

--- dsi_erp/hooks.py
+++ dsi_erp/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Siva"
 app_description = "Dsi Erp"
 app_email = "siva@enfono.in"
 app_license = "mit"
-
+fixtures= ["Client Script","Print Format"]
 # Apps
 # ------------------
 
@@ -44,10 +44,9 @@ app_license = "mit"
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-doctype_list_js = {"Interview" : "public/js/interview_list.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
 
 # Svg Icons
 # ------------------
@@ -146,17 +145,10 @@ doctype_list_js = {"Interview" : "public/js/interview_list.js"}
 # 	}
 # }
 doc_events = {
-    "*": {
-        "onload": "dsi_erp.restrictions.employee_restriction.restrict_top_level_employee_doc"
-    }
+	"Employee": {
+		"on_update": "dsi_erp.dsi_erp.doctype.renewable_document.renewable_document.on_employee_update"
+	}
 }
-# doc_events = {
-#     "*": {
-#         "onload": "dsi_erp.restrictions.tlm_hide.restrict_top_level_employee_doc",
-#         # "before_submit": "dsi_erp.restrictions.tlm_hide.restrict_top_level_employee_doc",
-#         # "validate": "your_app.path.to.restrict_top_level_employee_doc",
-#     }
-# }
 
 # Scheduled Tasks
 # ---------------
